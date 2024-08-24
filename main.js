2c80c66bda9e327b96b5c18112094cea39124ffd
KentaK <kenta.kawanami.coder@gmail.com>
2024-08-24 12:19:52 +0900

    js追加

---
+++ main.js
@@ -0,0 +1,19 @@
+const game = document.getElementById("game")
+const travel = document.getElementById("travel")
+const blacktea = document.getElementById("black-tea")
+
+game.onclick = function(){
+    scrollTo(0,922)
+
+}
+
+travel.onclick = function(){
+    scrollTo(0, 1555)
+
+}
+
+blacktea.onclick = function(){
+    scrollTo(0, 3000)
+
+
+}
